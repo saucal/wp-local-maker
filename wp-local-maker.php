6e1c5ff91df230bb2ecb66326df3ae4087b5ad2e
Matias Saggiorato <matiassaggiorato@gmail.com>
2018-06-21 17:23:33 -0300

    Initial commit

---
+++ wp-local-maker.php
@@ -0,0 +1,9 @@
+<?php
+ /**
+  * Plugin Name: WP Local Maker
+  * Plugin URI: https://www.saucal.com
+  * Description: WP CLI Exports with reduced datasets
+  * Version: 1.0.0
+  * Author: SAU/CAL
+  * Author URI: https://www.saucal.com
+  */
