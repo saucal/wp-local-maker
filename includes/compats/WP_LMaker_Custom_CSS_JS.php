8821f11e77d40bee409b5199bb09d2910f74d180
Matias Saggiorato <matiassaggiorato@gmail.com>
2019-03-06 13:17:44 -0300

    Add Custom CSS and JS compatibility

---
+++ includes/compats/WP_LMaker_Custom_CSS_JS.php
@@ -0,0 +1,13 @@
+<?php
+class WP_LMaker_Custom_CSS_JS extends WP_LMaker_Abstract_Addon {
+	function __construct() {
+		parent::__construct();
+        add_action( 'wp_local_maker_extra_compressed_paths', array( $this, 'custom_paths' ) );
+	}
+
+	function custom_paths( $paths ) {
+		$paths[] = 'wp-content/uploads/custom-css-js';
+		return $paths;
+	}
+}
+new WP_LMaker_Custom_CSS_JS();
