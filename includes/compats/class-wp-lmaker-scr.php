9dd126b74a184c0bb80ba0aad8cf2e177a95bc97
Matias Saggiorato <matiassaggiorato@gmail.com>
2019-10-30 21:36:00 -0300

    Make SCR more flexible (to add compatibility for previous versions)

--- includes/compats/class-wp-lmaker-scr.php
+++ includes/compats/class-wp-lmaker-scr.php
@@ -11,30 +11,36 @@
  */
 class WP_LMaker_SCR extends WP_LMaker_Abstract_Addon {
 
-	public function __construct() {
+	private $config = array();
+
+	public function __construct( $config = array() ) {
 		parent::__construct();
+		$this->config = (object) wp_parse_args( $config, array(
+			'scr_r' => 'scr_relationships',
+			'scr_rm' => 'scr_relationshipmeta',
+		) );
 		add_filter( 'wp_local_maker_custom_process_tables', array( $this, 'enqueue_process_scr' ), 45 );
 		add_filter( 'wp_local_maker_global_tables', array( $this, 'register_global_tables' ), 45 );
 	}
 
 	public function enqueue_process_scr( $tables ) {
 		global $wpdb;
-		$tables['scr_relationships']    = array( $this, 'process_scr_relationships' );
-		$tables['scr_relationshipmeta'] = array( $this, 'process_scr_relationshipmeta' );
+		$tables[$this->config->scr_r]    = array( $this, 'process_scr_relationships' );
+		$tables[$this->config->scr_rm] = array( $this, 'process_scr_relationshipmeta' );
 		return $tables;
 	}
 
 	public function register_global_tables( $tables ) {
-		$tables[] = 'scr_relationships';
-		$tables[] = 'scr_relationshipmeta';
+		$tables[] = $this->config->scr_r;
+		$tables[] = $this->config->scr_rm;
 		return $tables;
 	}
 
 	public function process_scr_relationships() {
 		global $wpdb;
 		$tables_info = Backup_Command::get_tables_names();
-		$current     = $tables_info['scr_relationships']['currname'];
-		$temp        = $tables_info['scr_relationships']['tempname'];
+		$current     = $tables_info[$this->config->scr_r]['currname'];
+		$temp        = $tables_info[$this->config->scr_r]['tempname'];
 
 		$wpdb->query( "CREATE TABLE IF NOT EXISTS {$temp} LIKE {$current}" );
 
@@ -44,7 +50,7 @@ class WP_LMaker_SCR extends WP_LMaker_Abstract_Addon {
 		// Export every matching relationship from a user standpoint
 		$wpdb->query(
 			"REPLACE INTO {$temp}
-			SELECT * FROM wp_scr_relationships scr 
+			SELECT * FROM {$current} scr 
 			WHERE 
 			scr.object1_type = 'user' AND scr.object1_site = 1 AND scr.object1_id IN ( SELECT ID FROM {$temp_users} )  
 			AND
@@ -54,7 +60,7 @@ class WP_LMaker_SCR extends WP_LMaker_Abstract_Addon {
 		// Export every matching relationship from a post standpoint
 		$wpdb->query(
 			"REPLACE INTO {$temp}
-			SELECT * FROM wp_scr_relationships scr 
+			SELECT * FROM {$current} scr 
 			WHERE 
 			scr.object1_type = 'post' AND scr.object1_site = " . get_current_blog_id() . " AND scr.object1_id IN ( SELECT ID FROM {$temp_posts} )
 			AND
@@ -68,7 +74,7 @@ class WP_LMaker_SCR extends WP_LMaker_Abstract_Addon {
 
 	public function process_scr_relationshipmeta() {
 		global $wpdb;
-		return Backup_Command::dependant_table_dump_single( 'scr_relationshipmeta', 'scr_relationships', 'scr_relationship_id', 'rel_id' );
+		return Backup_Command::dependant_table_dump_single( $this->config->scr_rm, $this->config->scr_r, 'scr_relationship_id', 'rel_id' );
 	}
 }
 
