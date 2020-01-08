87dd058c7691f6107c2a1c36e1e3721423d9ef36
Matias Saggiorato <matiassaggiorato@gmail.com>
2020-01-08 19:41:18 -0300

    Fix include paths warnings

--- includes/class-wp-lmaker-init-compats.php
+++ includes/class-wp-lmaker-init-compats.php
@@ -13,11 +13,11 @@ class WP_LMaker_Init_Compats {
 	public function __construct() {
 
 		// Addon Abstract Class
-		require_once 'class-wp-lmaker-abstract-addon.php';
+		require_once __DIR__ . '/class-wp-lmaker-abstract-addon.php';
 
 		$compats = glob( __DIR__ . '/compats/class-wp-lmaker*.php' );
 		foreach ( $compats as $compat ) {
-			require_once $compat;
+			require_once $compat; // phpcs:ignore WordPressVIPMinimum.Files.IncludingFile
 		}
 
 		do_action( 'wp_local_maker_init_compats' );
